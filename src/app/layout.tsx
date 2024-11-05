d89bd7be346f16695ddb31d63771d17ba76cdf31
w01f-o <arsenimarchenko@bk.ru>
2024-11-06 01:21:14 +0300

    fix: Removed initial fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,8 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./styles/globals.css";
-import { FC, ReactNode } from "react";
+import {FC, ReactNode} from "react";
 import clsx from "clsx";
 import RootProvider from "@/components/features/providers/RootProvider";
 import Layout from "@/components/pages/Layout";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -25,7 +12,7 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en" className="dark">
       <body
-        className={clsx(geistSans.variable, geistMono.variable, "antialiased")}
+        className={clsx("antialiased")}
       >
         <RootProvider>
           <Layout>{children}</Layout>
